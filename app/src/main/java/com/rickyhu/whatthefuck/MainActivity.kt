23e7cc8911584107505af9bf1ac5ddddec0625c7
Ricky Hu <ricky9667@gmail.com>
2020-05-07 21:35:55 +0800

    Add yes/no gif

--- app/src/main/java/com/rickyhu/whatthefuck/MainActivity.kt
+++ app/src/main/java/com/rickyhu/whatthefuck/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.rickyhu.whatthefuck
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
@@ -23,8 +30,10 @@ class MainActivity : AppCompatActivity() {
     fun onClick(view: View) {
 
         val answerTextView = findViewById<TextView>(R.id.answerTextView)
-        answerTextView.text = "??????"
+
+        answerTextView.text = "?"
         answerTextView.setTextColor(resources.getColor(android.R.color.black))
+        enableButtons(false)
 
         var wtfUrl = WTF_URL
         val button: Button = view as Button
@@ -61,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                 Log.i("WTF Image", imageUrl)
 
                 runOnUiThread {
-                    setResult(answer)
+                    loadImage(imageUrl, answer)
                 }
             }
         })
@@ -91,5 +100,67 @@ class MainActivity : AppCompatActivity() {
                 answerTextView.setTextColor(resources.getColor(android.R.color.black))
             }
         }
+    }
+
+    fun loadImage(url: String, answer: String) {
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        Glide.with(this)
+            .load(url)
+            .centerCrop()
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    setResult(answer)
+                    enableButtons(true)
+                    return false
+                }
+
+            })
+            .into(imageView)
+    }
+
+    fun enableButtons(enable: Boolean) {
+        val yesButton = findViewById<Button>(R.id.yesButton)
+        val maybeButton = findViewById<Button>(R.id.maybeButton)
+        val noButton = findViewById<Button>(R.id.noButton)
+        val randomButton = findViewById<Button>(R.id.randomButton)
+
+        if (enable) {
+            yesButton.isEnabled = true
+            maybeButton.isEnabled = true
+            noButton.isEnabled = true
+            randomButton.isEnabled = true
+
+            yesButton.isClickable = true
+            maybeButton.isClickable = true
+            noButton.isClickable = true
+            randomButton.isClickable = true
+        } else {
+            yesButton.isEnabled = false
+            maybeButton.isEnabled = false
+            noButton.isEnabled = false
+            randomButton.isEnabled = false
+
+
+            yesButton.isClickable = false
+            maybeButton.isClickable = false
+            noButton.isClickable = false
+            randomButton.isClickable = false
+        }
+
     }
 }
